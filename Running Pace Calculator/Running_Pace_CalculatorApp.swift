d6ae385a4ed3c10ab0092c50a90ea2f4b0489161
paulcalver <paul@paulcalver.cc>
2025-11-25 19:49:26 +0000

    Initial Commit

---
+++ Running Pace Calculator/Running_Pace_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Running_Pace_CalculatorApp.swift
+//  Running Pace Calculator
+//
+//  Created by Paul Calver on 25/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct Running_Pace_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
